d46d9bc6e651959278536385bda02a6ee432528f
Patryk <patlaw96@gmail.com>
2014-10-17 13:11:51 +0200

    Project init

---
+++ gen/ugteamproject/com/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ugteamproject.com;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/ugteamproject/com/MainActivity.java
@@ -0,0 +1,34 @@
+package ugteamproject.com;
+
+import org.andengine.engine.camera.Camera;
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.engine.options.ScreenOrientation;
+import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.andengine.entity.scene.Scene;
+import org.andengine.entity.scene.background.Background;
+import org.andengine.ui.activity.SimpleBaseGameActivity;
+
+public class MainActivity extends SimpleBaseGameActivity {
+	 
+    static final int CAMERA_WIDTH = 1280;
+    static final int CAMERA_HEIGHT = 768;
+ 
+    @Override
+    public EngineOptions onCreateEngineOptions() {
+        Camera mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+        return new EngineOptions(true, ScreenOrientation.LANDSCAPE_SENSOR,
+            new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), mCamera);
+    }
+ 
+    @Override
+    protected void onCreateResources() {
+        // TODO Auto-generated method stub
+    }
+ 
+    @Override
+    protected Scene onCreateScene() {
+        Scene scene = new Scene();
+        scene.setBackground(new Background(0.09804f, 0.6274f, 0));
+        return scene;
+    }
+}
