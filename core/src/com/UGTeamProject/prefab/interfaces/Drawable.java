869fbb614b574ac430ccfa9f47cf3419eb9bcc8d
Jacek Sikora <jaresh@wp.pl>
2014-11-16 20:47:47 +0100

    #4 Drawable interface

--- core/src/com/UGTeamProject/prefab/interfaces/Drawable.java
+++ core/src/com/UGTeamProject/prefab/interfaces/Drawable.java
@@ -1,5 +1,10 @@
 package com.UGTeamProject.prefab.interfaces;
+import com.badlogic.gdx.graphics.g2d.Batch;
 
 public interface Drawable {
+	
+	public void draw(Batch batcher);
+	public void draw(Batch batch, float x, float y, float angle);
+	public void draw(Batch batch, float x, float y);
 
 }
