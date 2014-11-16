057c24da92fb8ae292c65ec7678879d57f3e4c93
Jacek Sikora <jaresh@wp.pl>
2014-11-16 21:31:38 +0100

    Two lists and constructor.

--- core/src/com/UGTeamProject/prefab/GameObject.java
+++ core/src/com/UGTeamProject/prefab/GameObject.java
@@ -1,5 +1,23 @@
 package com.UGTeamProject.prefab;
+import java.util.ArrayList;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Texture;
 
 public class GameObject {
+	
+	public static ArrayList<Sound> sound = new ArrayList<Sound>();
+	public static ArrayList<Texture> texture = new ArrayList<Texture>();
+	
+	public static Texture item;
+
+	GameObject(String textureName,String soundName){
+		
+		item = new Texture(Gdx.files.internal(textureName));
+		
+	    texture.add(item);
+	    sound.add(Gdx.audio.newSound(Gdx.files.internal(soundName)));
+	}
 
 }
