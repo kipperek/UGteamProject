1cd78a97a4f6ebb73219e87b02de0f3e61e3df2c
Kacper Czechowicz <czechowicz.kacper@gmail.com>
2014-11-16 21:35:35 +0200

    first steps to draw textures

--- core/src/com/UGTeamProject/prefab/adapters/Texture.java
+++ core/src/com/UGTeamProject/prefab/adapters/Texture.java
@@ -1,5 +1,40 @@
 package com.UGTeamProject.prefab.adapters;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
+
 public class Texture {
+	
+	private Sprite texture;
+	
+	public Texture (String resourceName, Vector2 origin, Vector2 position){
+		texture = new Sprite(new com.badlogic.gdx.graphics.Texture(Gdx.files.internal(resourceName)));
+		texture.setOrigin(origin.x,origin.y);
+		texture.setPosition(position.x, position.y);
+		
+	}
+	public Texture (String resourceName, Vector2 origin){
+		texture = new Sprite(new com.badlogic.gdx.graphics.Texture(Gdx.files.internal(resourceName)));
+		texture.setOrigin(origin.x,origin.y);
+	}
+	public Texture (String resourceName){
+		texture = new Sprite(new com.badlogic.gdx.graphics.Texture(Gdx.files.internal(resourceName)));
+	}
+	
+	public void draw(Batch batch, float x, float y, float angle){
+		texture.setRotation(angle);
+		texture.setPosition(x, y);
+		texture.draw(batch);
+		
+	}
+	public void draw(Batch batch, float x, float y){
+		texture.setPosition(x, y);
+		texture.draw(batch);
+	}
+	public void draw(Batch batch){
+		texture.draw(batch);
+	}
 
 }
