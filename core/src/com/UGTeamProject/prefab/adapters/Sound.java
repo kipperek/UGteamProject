49b88447ce2a9cc06a2ce353d8bef7fdc31c32df
Marcin Ossowski <lcinek18@gmail.com>
2014-11-21 17:42:12 +0100

    #2  Sound Class - added distance function

--- core/src/com/UGTeamProject/prefab/adapters/Sound.java
+++ core/src/com/UGTeamProject/prefab/adapters/Sound.java
@@ -1,5 +1,51 @@
 package com.UGTeamProject.prefab.adapters;
 
-public class Sound {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Vector2;
 
+public class Sound {
+	
+	private com.badlogic.gdx.audio.Sound sound;
+	private Vector2 position;
+	private int hearDistance = 300;
+	
+	public Sound (String resourceName, Vector2 soundPosition) {
+		sound = Gdx.audio.newSound(Gdx.files.internal(resourceName));
+		position = new Vector2();
+		position.x = soundPosition.x;
+		position.y = soundPosition.y;
+	}
+	
+	public Sound (String resourceName) {
+		sound = Gdx.audio.newSound(Gdx.files.internal(resourceName));
+	}
+	
+	public void play() {
+		sound.play();
+	}
+	
+	public void play(Vector2 soundPosition, Vector2 charPosition) {
+		
+		//volume range 0 - 1
+		float distance = 0;
+		
+		//counting max distance
+		if ((soundPosition.x - charPosition.x) > distance)
+			distance = soundPosition.x - charPosition.x;
+		else 
+			distance = charPosition.x - soundPosition.x ;
+		
+		if ((soundPosition.y - charPosition.y) > distance)
+			distance = soundPosition.y - charPosition.y;
+		else 
+			distance = charPosition.y - soundPosition.y;
+		
+		//handling distance
+		if (distance > hearDistance)
+			sound.play(0f);
+		else if (distance > hearDistance/2)
+			sound.play(0.1f);
+		else
+			sound.play(1f);	
+	}	
 }
