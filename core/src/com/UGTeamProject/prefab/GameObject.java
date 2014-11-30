759b38f159a546f3b08e65ade48b27acba5a4d9e
Jacek Sikora <jaresh@wp.pl>
2014-11-30 10:51:01 +0100

    #10 Changes in GameObject class

--- core/src/com/UGTeamProject/prefab/GameObject.java
+++ core/src/com/UGTeamProject/prefab/GameObject.java
@@ -1,17 +1,38 @@
 package com.UGTeamProject.prefab;
 
 import java.util.ArrayList;
+
 import com.UGTeamProject.prefab.adapters.Physics;
 import com.UGTeamProject.prefab.adapters.Sound;
 import com.UGTeamProject.prefab.adapters.Texture;
 import com.UGTeamProject.prefab.adapters.Music;
+import com.badlogic.gdx.math.Vector2;
 
 public class GameObject {
 	
+	public static ArrayList<GameObject> gameObjects = new ArrayList<GameObject>();
+	
+	private Vector2 position;
 	public static ArrayList<Sound> sound = new ArrayList<Sound>();
 	public static ArrayList<Texture> texture = new ArrayList<Texture>();
-	public static ArrayList<Physics> physics = new ArrayList<Physics>();
+	public static ArrayList<Physics> physic = new ArrayList<Physics>();
 	public static ArrayList<Music> music = new ArrayList<Music>();
+	
+	GameObject(ArrayList<Sound> sounds, ArrayList<Texture> textures, 
+			   ArrayList<Physics> physics, ArrayList<Music> musics){
+		
+		sound = sounds;
+		physic = physics;
+		texture = textures;
+		music = musics;
+	}
+	
+	public void addGameObject(GameObject newObject){
+		gameObjects.add(newObject);
+	}
+	public void removeGameObject(GameObject newObject){
+		gameObjects.remove(newObject);
+	}
 	
 	public void addTexture(Texture newTexture){
 		texture.add(newTexture);
@@ -27,12 +48,12 @@ public class GameObject {
 		sound.remove(newSound);
 	}
 	
-	public void addPhysics(Physics newPhysics){
-		physics.add(newPhysics);
+	public void addPhysic(Physics newPhysic){
+		physic.add(newPhysic);
 	}
 	
-	public void removePhysics(Physics newPhysics){
-		physics.remove(newPhysics);
+	public void removePhysic(Physics newPhysic){
+		physic.remove(newPhysic);
 	}
 	
 	public void addMusic(Music newMusic){
@@ -42,5 +63,10 @@ public class GameObject {
 	public void removeMusic(Music newMusic){
 		music.remove(newMusic);
 	}
+	
+	
+	public void update(){} 
+	
+	public void draw(){}
 
 }
