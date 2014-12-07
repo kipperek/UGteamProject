2b4b19aafab43dd0fdf7973c44a37d24a6e6f5f3
Marcin Ossowski <lcinek18@gmail.com>
2014-12-07 17:09:24 +0100

    #13 Actor class - added width and height functions

--- core/src/com/UGTeamProject/actor/Actor.java
+++ core/src/com/UGTeamProject/actor/Actor.java
@@ -25,4 +25,20 @@ public class Actor {
 	public void setY(float y) {
 		actor.y = y;
 	}
+	
+	public float getWidth() {
+		return actor.width;
+	}
+	
+	public void setWidth(float width) {
+		actor.width = width;
+	}
+	
+	public float getHeight() {
+		return actor.height;
+	}
+	
+	public void setHeight(float height) {
+		actor.height = height;
+	}
 }
