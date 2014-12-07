95575e0e88469d5e31ed7c2edaa82fc3b23cde9c
Marcin Ossowski <lcinek18@gmail.com>
2014-12-07 16:05:02 +0100

    #14 Character class - fixed

--- core/src/com/UGTeamProject/actor/Character.java
+++ core/src/com/UGTeamProject/actor/Character.java
@@ -1,38 +1,17 @@
 package com.UGTeamProject.actor;
 
 import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Rectangle;
 
+public class Character extends Actor {
 
-public class Character {
-
-	private Rectangle player;
-	
 	public Character() {
-		player = new Rectangle();
 		spawn();
-		player.width = 64;
-		player.height = 64;
+		actor.width = 64;
+		actor.height = 64;
 	}
 	
 	public void spawn() {
-		player.x = MathUtils.random(0,1000);
-		player.y = MathUtils.random(0,1000); 
-	}
-	
-	public float getX() {
-		return player.x;
-	}
-	
-	public void setX(float x) {
-		player.x = x;
-	}
-	
-	public float getY() {
-		return player.y;
-	}
-	
-	public void setY(float y) {
-		player.y = y;
+		actor.x = MathUtils.random(0,1000);
+		actor.y = MathUtils.random(0,1000); 
 	}
 }
