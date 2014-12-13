b3f74852a576a5eda9ca22dc80735f64d153295c
Marcin Ossowski <lcinek18@gmail.com>
2014-12-13 22:18:19 +0100

    Update AssetsManager.java

--- core/src/com/UGTeamProject/game/AssetsManager.java
+++ core/src/com/UGTeamProject/game/AssetsManager.java
@@ -6,14 +6,12 @@ import com.UGTeamProject.prefab.adapters.Texture;
 public class AssetsManager {
 
 	public static Texture playerTexture;
-	public static Texture grassTexture;
 	public static Texture radioTexture;
 	public static Music radioMusic;
 	
 	public static void load() {
 		
 		playerTexture = new Texture("sprites/bohater.png");
-		grassTexture = new Texture("sprites/grass.png");
 		radioTexture = new Texture("sprites/radio.jpg");
 		radioMusic = new Music("music/radio.mp3"); 
 	}
