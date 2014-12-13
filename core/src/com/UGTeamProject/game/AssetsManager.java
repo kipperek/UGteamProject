f3d3de41cc0d76775591caedae47ac7e654d6ba3
Marcin Ossowski <lcinek18@gmail.com>
2014-12-13 22:24:21 +0100

    #19 AssetsManager class - added radio

--- core/src/com/UGTeamProject/game/AssetsManager.java
+++ core/src/com/UGTeamProject/game/AssetsManager.java
@@ -1,16 +1,20 @@
 package com.UGTeamProject.game;
 
+import com.UGTeamProject.prefab.adapters.Music;
 import com.UGTeamProject.prefab.adapters.Texture;
 
 public class AssetsManager {
 
 	public static Texture playerTexture;
 	public static Texture grassTexture;
+	public static Texture radioTexture;
+	public static Music radioMusic;
 	
 	public static void load() {
 		
 		playerTexture = new Texture("sprites/bohater.png");
 		grassTexture = new Texture("sprites/grass.png");
-		
+		radioTexture = new Texture("sprites/radio.jpg");
+		radioMusic = new Music("music/radio.mp3"); 
 	}
 }
