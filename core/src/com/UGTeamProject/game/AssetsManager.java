046e1e97bab83bc11bc5c034082fcfbaa219da3b
Marcin Ossowski <lcinek18@gmail.com>
2014-12-21 13:56:04 +0100

    #19 Assets Manager class - added npc texture

--- core/src/com/UGTeamProject/game/AssetsManager.java
+++ core/src/com/UGTeamProject/game/AssetsManager.java
@@ -4,12 +4,12 @@ import com.UGTeamProject.prefab.adapters.Music;
 import com.UGTeamProject.prefab.adapters.Texture;
 import com.badlogic.gdx.math.Vector2;
 
-
 public class AssetsManager {
 
 	public static Texture playerTexture;
 	public static Texture grassTexture;
 	public static Texture radioTexture;
+	public static Texture npcTexture;
 	public static com.badlogic.gdx.graphics.Texture touchBackground;
 	public static com.badlogic.gdx.graphics.Texture touchKnob;
 	public static Music radioMusic;
@@ -19,6 +19,7 @@ public class AssetsManager {
 		playerTexture = new Texture("sprites/bohater.png");
 		grassTexture = new Texture("sprites/grass.png");
 		radioTexture = new Texture("sprites/radio.jpg");
+		npcTexture = new Texture("sprites/npc.png");
 		touchBackground = new com.badlogic.gdx.graphics.Texture("sprites/touchBackground.png");
 	    touchKnob = new com.badlogic.gdx.graphics.Texture("sprites/touchKnob.png");
 		radioMusic = new Music("music/radio.mp3"); 
