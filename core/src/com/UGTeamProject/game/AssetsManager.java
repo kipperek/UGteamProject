6e74aa67659973f59946024283ac1b3dd4ab2209
Marcin Ossowski <lcinek18@gmail.com>
2014-12-21 14:06:02 +0100

    Update AssetsManager.java

--- core/src/com/UGTeamProject/game/AssetsManager.java
+++ core/src/com/UGTeamProject/game/AssetsManager.java
@@ -2,19 +2,26 @@ package com.UGTeamProject.game;
 
 import com.UGTeamProject.prefab.adapters.Music;
 import com.UGTeamProject.prefab.adapters.Texture;
+import com.badlogic.gdx.math.Vector2;
 
 public class AssetsManager {
 
 	public static Texture playerTexture;
 	public static Texture grassTexture;
 	public static Texture radioTexture;
+	public static Texture npcTexture;
+	public static com.badlogic.gdx.graphics.Texture touchBackground;
+	public static com.badlogic.gdx.graphics.Texture touchKnob;
 	public static Music radioMusic;
 	
 	public static void load() {
 		
-		playerTexture = new Texture("sprites/bohater.png");
-		grassTexture = new Texture("sprites/grass.png");
-		radioTexture = new Texture("sprites/radio.jpg");
-		radioMusic = new Music("music/radio.mp3"); 
+	    playerTexture = new Texture("sprites/bohater.png");
+	    grassTexture = new Texture("sprites/grass.png");
+	    radioTexture = new Texture("sprites/radio.jpg");
+	    npcTexture = new Texture("sprites/npc.png");
+	    touchBackground = new com.badlogic.gdx.graphics.Texture("sprites/touchBackground.png");
+	    touchKnob = new com.badlogic.gdx.graphics.Texture("sprites/touchKnob.png");
+	    radioMusic = new Music("music/radio.mp3"); 
 	}
 }
