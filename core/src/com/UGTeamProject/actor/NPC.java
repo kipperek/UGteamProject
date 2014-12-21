1d3d8a7dc44fcd8a47fdd1c4b824636e950012bf
Marcin Ossowski <lcinek18@gmail.com>
2014-12-21 13:51:43 +0100

    #15 Npc class - added act,die and spawn functions

--- core/src/com/UGTeamProject/actor/NPC.java
+++ core/src/com/UGTeamProject/actor/NPC.java
@@ -1,5 +1,35 @@
 package com.UGTeamProject.actor;
 
-public class NPC {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.MathUtils;
 
+public class NPC extends Actor {
+
+	public NPC() {
+		spawn();
+		actor.width = 64;
+		actor.height = 64;
+	}
+	
+	public void spawn() {
+		actor.x = MathUtils.random(500,2500);
+		actor.y = MathUtils.random(500,2500); 
+	}
+	
+	public void die() {
+		if(life <= 0)
+	        spawn();
+	}
+	
+	public void act(Character player) {
+		 if(player.getY() > actor.y)
+			 actor.y += 100 * Gdx.graphics.getDeltaTime();
+         else
+        	 actor.y -= 100 * Gdx.graphics.getDeltaTime();
+         
+         if(player.getX() > actor.x)
+        	 actor.x += 100 * Gdx.graphics.getDeltaTime();
+         else
+        	 actor.x -= 100 * Gdx.graphics.getDeltaTime();
+	}
 }
