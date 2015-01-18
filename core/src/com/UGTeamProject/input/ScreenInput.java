c56ad87a9d20762479157885354129d5950e4d62
Jacek Sikora <jaresh@wp.pl>
2015-01-18 21:43:39 +0100

    Revert "#17 analog placement and position change!"
    This reverts commit 15e15dc25c2cfa2283b368a7ed25c973c82cb443.

--- core/src/com/UGTeamProject/input/ScreenInput.java
+++ core/src/com/UGTeamProject/input/ScreenInput.java
@@ -3,20 +3,21 @@ package com.UGTeamProject.input;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Touchpad;
 import com.badlogic.gdx.scenes.scene2d.ui.Touchpad.TouchpadStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
-import com.UGTeamProject.actor.Character;
+import com.UGTeamProject.actor.Actor;
 import com.UGTeamProject.game.AssetsManager;
 
 public class ScreenInput {
 	
-	public static Character player;
+	public static Actor player;
 	Vector2 vec = new Vector2();
 	Vector2 vec2 = new Vector2();
 
-	public ScreenInput(Character newPlayer)
+	public ScreenInput(Actor newPlayer)
 	{
 		player = newPlayer;
 	}
@@ -38,7 +39,7 @@ public class ScreenInput {
         touchpadStyle.background = touchBackground;
         touchpadStyle.knob = touchKnob;
         touchpad = new Touchpad(1, touchpadStyle);
-        touchpad.setBounds(x - 150, y - 50, 83, 83);
+        touchpad.setBounds(x - 150, y - 50, 50, 50);
         
 		return touchpad;
 	}
@@ -48,34 +49,25 @@ public class ScreenInput {
 		player.setX(player.getX() + leftAnalog.getKnobPercentX()*5);
 		leftAnalog.setX((player.getX() - 150) + leftAnalog.getKnobPercentX()*5);
         player.setY(player.getY() + leftAnalog.getKnobPercentY()*5);
-        leftAnalog.setY((player.getY() - 90) + leftAnalog.getKnobPercentY()*5);
+        leftAnalog.setY((player.getY() - 50) + leftAnalog.getKnobPercentY()*5);
         
         vec.set(rightAnalog.getKnobPercentX(), rightAnalog.getKnobPercentY());
         player.setRotation(vec.angle()+270);
         
         rightAnalog.setX((player.getX() + 150) + rightAnalog.getKnobPercentX()*5);
-        rightAnalog.setY((player.getY() - 90) + rightAnalog.getKnobPercentY()*5);
+        rightAnalog.setY((player.getY() - 50) + rightAnalog.getKnobPercentY()*5);
 	}
 	public void listen()
 	{
 		
-		if (Gdx.input.isKeyPressed(Keys.A))
-			player.setX(player.getX() - 200 * Gdx.graphics.getDeltaTime());
-		if (Gdx.input.isKeyPressed(Keys.D))
-			player.setX(player.getX() + 200 * Gdx.graphics.getDeltaTime());
-		if (Gdx.input.isKeyPressed(Keys.W))
-			player.setY(player.getY() + 200 * Gdx.graphics.getDeltaTime());
-		if (Gdx.input.isKeyPressed(Keys.S))
-			player.setY(player.getY() - 200 * Gdx.graphics.getDeltaTime());
-		if (Gdx.input.isKeyJustPressed(Keys.F))
-			player.pickUp();
-		if (Gdx.input.isKeyJustPressed(Keys.Q))
-			player.changeWeapon();
-
-		float mouseX = Gdx.input.getX();
-		float mouseY = Gdx.input.getY();
-		player.setRotation((float) Math.toDegrees(Math.atan2(-mouseY + Gdx.graphics.getHeight() / 2, mouseX - Gdx.graphics.getWidth() / 2)) - 90);
-
+		if(Gdx.input.isKeyPressed(Keys.LEFT)) 
+	        player.setX(player.getX() - 200 * Gdx.graphics.getDeltaTime());
+		if(Gdx.input.isKeyPressed(Keys.RIGHT)) 
+	        player.setX(player.getX() + 200 * Gdx.graphics.getDeltaTime());
+		if(Gdx.input.isKeyPressed(Keys.UP)) 
+	        player.setY(player.getY() + 200 * Gdx.graphics.getDeltaTime());
+		if(Gdx.input.isKeyPressed(Keys.DOWN)) 
+	        player.setY(player.getY() - 200 * Gdx.graphics.getDeltaTime());
 	    
 	}
 }
