e8857eddc2e9806b80228c0c3db326574b731a38
Marcin Ossowski <lcinek18@gmail.com>
2015-01-26 00:25:31 +0100

    #28 Actor Manger - npcs

--- core/src/com/UGTeamProject/game/ActorManager.java
+++ core/src/com/UGTeamProject/game/ActorManager.java
@@ -6,24 +6,16 @@ import com.UGTeamProject.actor.NPC;
 
 public class ActorManager {
 	
-	public static NPC npc1, npc2, npc3, npc4, npc5, npc6;
+	public static NPC npc;
 	public static ArrayList<NPC> npcs;
 	
 	public static void load() {
-		npc1 = new NPC(AssetsManager.npcTexture);
-		npc2 = new NPC(AssetsManager.npcTexture);
-		npc3 = new NPC(AssetsManager.npcTexture);
-		npc4 = new NPC(AssetsManager.npcTexture);
-		npc5 = new NPC(AssetsManager.npcTexture);
-		npc6 = new NPC(AssetsManager.npcTexture);
 		
 		npcs = new ArrayList<NPC>();
 		
-		npcs.add(npc1);
-		npcs.add(npc2);
-		npcs.add(npc3);
-		npcs.add(npc4);
-		npcs.add(npc5);
-		npcs.add(npc6);
+		for(int i=0; i<10; i++) {
+		    npc = new NPC(AssetsManager.npcTexture);
+			npcs.add(npc);
+		}
 	}
 }
